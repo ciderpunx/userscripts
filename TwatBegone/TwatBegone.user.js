747d88e8cb06d0be0cd71e064bc5d71345f04d23
Charlie Harvey <charlie@mallory.hendred.net>
2017-03-29 22:08:22 +0100

    Started work on TwatBegone project
    The idea is that you can remove all mention of @realDonaldTrump
    from your twitter, though it may evolve to replace him with
    something else.

---
+++ TwatBegone/TwatBegone.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name        TwatBegone
+// @namespace   ox4
+// @description Removes twats and all mentions of them from twitter
+// @include     https://twitter.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+twats = [/fake news/i, /nytimes/i];
+
+
+tweets=document.getElementsByClassName("stream-item");
+
+console.debug(tweets.length);
+
+for (var i=1; i<=tweets.length; i++) {
+  var tc = tweets[i].textContent;
+  console.debug(i + tc.replace(/\s+/g, ' '));
+  if (anyMatch(tc)) {
+    console.debug("Tweet" + i + " will be removed");
+    tweets[i].parentNode.removeChild(tweets[i]);
+    i--; // yes, this is disgusting
+  }
+}
+
+function anyMatch(tc) {
+
+  for(var i=0;i<twats.length;i++) {
+    if(tc.match(twats[i])!==null) {
+      return 1; // tc.match(twats[i]);
+    }
+  }
+  return 0;
+}
