c92ba9e75c613c0537eccdaa60790b05f20a66ac
Charlie Harvey <charlie@mallory.hendred.net>
2017-03-31 20:19:51 +0100

    Twats now configurable via the config page
    Also add a little status icon in the bottom left letting you know twatbegone is active and providing a link to the config page.
    And started work on the bit that will replace twats with kittens

--- TwatBegone/TwatBegone.user.js
+++ TwatBegone/TwatBegone.user.js
@@ -3,31 +3,92 @@
 // @namespace   ox4
 // @description Removes twats and all mentions of them from twitter
 // @include     https://twitter.com/*
-// @version     1
-// @grant       none
+// @version     0.1
+// @grant       GM_getResourceText
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @resource    configPage8 https://raw.githubusercontent.com/ciderpunx/userscripts/master/TwatBegone/configPage.html
 // ==/UserScript==
 
-twats = [/fake news/i, /nytimes/i];
+defaultTwats = ["realDonaldTrump","Nigel_Farage"];
 
-
-tweets=document.getElementsByClassName("stream-item");
-
-// console.debug(tweets.length);
-
-for (var i=1; i<=tweets.length; i++) {
-  var tc = tweets[i].textContent;
-  // console.debug(i + tc.replace(/\s+/g, ' '));
-  if (anyMatch(tc)) {
-    // console.debug("Tweet" + i + " will be removed");
-    tweets[i].parentNode.removeChild(tweets[i]);
-    i--; // yes, this is disgusting
-  }
+if(document.location.href=="https://twitter.com/?twatconf") {
+  showConfigPage();
+}
+else {
+  showIndicator();
+  twatBegone();
+  //twatToKitteh();
 }
 
+function showConfigPage() {
+  document.documentElement.innerHTML = GM_getResourceText("configPage8");
+  initTwats();
+  addSubmitListener();
+}
+
+function initTwats() {
+  twats = GM_getValue("twats", defaultTwats.join("\n"));
+  document.getElementById('twats').value = twats; 
+  setTwats(twats);
+}
+
+function addSubmitListener() {
+  document.getElementById("submit").addEventListener('click',updateSettings,false);
+}
+
+function setTwats(twats){
+  console.log("attempt to set twats to" + twats);
+  GM_setValue("twats",twats);
+}
+
+function updateSettings() {
+   setTwats(document.getElementById('twats').value);
+}
+
+// Makes a little fixed div at the bottom left with a link to the config page
+function showIndicator() {
+  var divv = document.createElement('div');
+  divv.innerHTML = '<a href="https://twitter.com/?twatconf" style="color:#fff">TwatBegone Active</a>';
+  divv.style.cssText = 'position:fixed;width:10em;height:2em;text-align:center;z-index:100;background:#f00;line-height:2em;left:0;bottom:0;font-family:freesans, helvetica, arial, sans-serif;';
+  document.getElementById('page-outer').appendChild(divv);
+}
+
+// Remove twats from twitter feed
+function twatBegone() {
+  twatProcess( function(tweet) { tweet.parentNode.removeChild(tweet); });
+}
+
+// Replace tweet with a cat from thecatapi.com
+// TODO: waiting to see if this will work as I'd like
+function twatToKitteh() {
+  twatProcess( function(tweet) { 
+    tweet.innerHTML = '<p>YES?<a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif"></a></p>'; 
+  });
+  console.log("ttk");
+
+}
+
+// Takes a function and apply it to any tweets that match one of the current twats
+function twatProcess(action) {
+  tweets = document.getElementsByClassName("stream-item");
+
+  for (var i=1; i<=tweets.length; i++) {
+   var tc = tweets[i].textContent;
+   if (anyMatch(tc)) {
+     action(tweets[i]);
+     i--; // yes, this is disgusting
+   }
+  } 
+}
+
+
 function anyMatch(tc) {
+  twats = GM_getValue("twats", defaultTwats.join("\n")).split("\n");
   for(var i=0;i<twats.length;i++) {
-    if(tc.match(twats[i])!==null) {
-      return 1; // tc.match(twats[i]);
+    t = new RegExp(twats[i],'i');
+    if(tc.match(t)!==null) {
+      return 1;
     }
   }
   return 0;
