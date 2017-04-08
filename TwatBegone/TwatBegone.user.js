2356b5470c761d87b470a49754828a1106934176
Charlie Harvey <charlie@mallory.hendred.net>
2017-04-08 12:03:30 +0100

    Refactor to tidy code up a bit
    Various tweaks (comments var declarations, etc)

--- TwatBegone/TwatBegone.user.js
+++ TwatBegone/TwatBegone.user.js
@@ -4,7 +4,7 @@
 // @namespace   ox4
 // @description Removes twats and all mentions of them from twitter
 // @include     https://twitter.com/*
-// @version     0.31
+// @version     0.32
 // @grant       GM_getResourceText
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getValue
@@ -24,6 +24,10 @@ var kittehs = GM_getResourceText("kittehs").split("\n");
 
 var haikuWords = GM_getResourceText("haiku").split(/\s+/);
 
+var fortuneServerURL = "https://helloacm.com/api/fortune/";
+
+var fortuneServerTimeout = 800;
+
 if(   document.location.href=="https://twitter.com/?twatconf"
    || document.location.href=="https://twitter.com/?twatconf#" ) {
   showConfigPage();
@@ -32,7 +36,7 @@ if(   document.location.href=="https://twitter.com/?twatconf"
 }
 else {
   showIndicator();
-  action = GM_getValue("action", "TwatBegone");
+  var action = GM_getValue("action", "TwatBegone");
   if (action=="TwatBegone") {
     twatBegone();
   }
@@ -52,27 +56,17 @@ function showConfigPage() {
 }
 
 function initAction() {
-  action = GM_getValue("action", "TwatBegone");
-  document.getElementById('action').value = action;
+  document.getElementById('action').value = GM_getValue("action", "TwatBegone");
   setAction(action);
 }
 
 function initTwats() {
-  twats = GM_getValue("twats", defaultTwats.join("\n"));
-  document.getElementById('twats').value = cleanTwats(twats);
+  document.getElementById('twats').value = cleanTwats(GM_getValue("twats", defaultTwats.join("\n")));
   setTwats(twats);
 }
 
 function addSubmitListener() {
   document.getElementById("submit").addEventListener('click',updateSettings,false);
-}
-
-function addShowUnimplementedListener() {
-  document.getElementById("showUnimplemented").addEventListener(
-      'click'
-      , function() { document.getElementById('unimplemented').style.display = 'inline'; }
-      , false
-  );
 }
 
 function setTwats(twats){
@@ -87,6 +81,7 @@ function setAction(action){
   GM_setValue("action",action);
 }
 
+// Called when user clicks updateSettings button on config page
 function updateSettings() {
    setTwats(document.getElementById('twats').value);
    setAction(document.getElementById('action').value);
@@ -96,7 +91,7 @@ function updateSettings() {
    info.style.display="inline";
 }
 
-// Makes a little fixed div at the bottom left with a link to the config page
+// Makes a little fixed div at the bottom right with a link to the config page, so ppl know TwatBegone is running
 function showIndicator() {
   var divv = document.createElement('div');
   divv.innerHTML = '<img style="float:left;height:25px;padding-top:2px" src="https://pbs.twimg.com/profile_images/850405364067688448/3x0b2zmz_normal.jpg" /><a href="https://twitter.com/?twatconf" style="color:#fff">TwatBegone Active</a>';
@@ -104,28 +99,28 @@ function showIndicator() {
   document.getElementById('page-outer').appendChild(divv);
 }
 
-// Remove twats from twitter feed
+// Action: Remove all mention of twats from twitter feed
 function twatBegone() {
   twatProcess( function(tweet) { tweet.parentNode.removeChild(tweet); }, true, true );
 }
 
-// Replace tweet with a cat from thecatapi.com
-// For some reason images do not work if they originate at a non-twitter domain
+// Action: Replace tweet with a cat from thecatapi.com
+// TODO: For some reason images do not work if they originate at a non-twitter domain
 function twatToKitteh() {
   twatProcess( function(tweet) {
-    mahKitteh = kittehs[Math.floor(Math.random() * kittehs.length)];
+    var mahKitteh = kittehs[Math.floor(Math.random() * kittehs.length)];
     tweet.innerHTML = '<p><em>The URL is <br />"' + mahKitteh + '"</em><br /><img src="' + mahKitteh + '" alt="kitteh"></p>'; 
   }, true);
 }
 
+// Action: replace with fortune from the server
 function twatFortune() {
-    fortuneServerURL = "https://helloacm.com/api/fortune/";
     twatProcess (function(tweet) {
-      fortune = "cannot load fortune";
+      var fortune = "cannot load fortune";
       GM_xmlhttpRequest({
         method: "GET",
         url: fortuneServerURL,
-        timeout: 800,
+        timeout: fortuneServerTimeout,
         onload: function(response) {
           fortune = response.responseText
                             .replace(/["]/g,'')
@@ -138,18 +133,19 @@ function twatFortune() {
     }, true);
 }
 
+// Action: replace with Haiku
 function twatHaiku() {
     twatProcess (function(tweet) {
-      haiku = "cannot compose haiku";
+      var haiku = "cannot compose haiku";
       haiku = makeHaiku();
       tweet.innerHTML =
           textTweet( 'Here&#8217;s a haiku made of words from <a href="https://raw.githubusercontent.com/herval/haikuzao/master/inputs/haiku.txt">these ones</a>.'
                    , haiku
-          )
-      ;
+                   );
     }, true);
 }
 
+// Given a title and a text, return something that can be used as a tweet, at least on desktop.
 function textTweet(title, txt) {
       return [ '<div style="border-bottom:1px solid #eee;padding:1em 0">'
              , '<img class="avatar" style="float:left;margin-left:12px" src="https://pbs.twimg.com/profile_images/850405364067688448/3x0b2zmz_bigger.jpg" alt="TBG" />'
@@ -159,9 +155,9 @@ function textTweet(title, txt) {
              ].join('');
 }
 
-// Takes a function and apply it to any tweets that match one of the current twats
+// Take a function and apply it to any tweets that match one of the current twats
 function twatProcess(action, firstCall, destructiveUpdate) {
-  tweets = document.getElementsByClassName("js-stream-item");
+  var tweets = document.getElementsByClassName("js-stream-item");
 
   if(firstCall) { //&& document.location.href=="https://twitter.com/"){
     document.body.onkeydown = function (e) {
@@ -183,7 +179,7 @@ function twatProcess(action, firstCall, destructiveUpdate) {
   }
 }
 
-// call a function at some set delays, to catch TL on twitter
+// Call a function at some set delays, to catch TL on twitter
 // 300ms, 1000.. step 1000
 function delayedCalls(fn) {
   setTimeout(fn, 300); // for really fast connections
@@ -192,16 +188,17 @@ function delayedCalls(fn) {
 
 // Check text content doesn't contain any mentions of twats
 function anyMatch(tc) {
-  twats = GM_getValue("twats", defaultTwats.join("\n")).split("\n").filter(nonempty);
+  var twats = GM_getValue("twats", defaultTwats.join("\n")).split("\n").filter(nonempty);
   for(var i=0;i<twats.length;i++) {
-    t = new RegExp(twats[i],'i');
+    var t = new RegExp(twats[i],'i');
     if(tc.match(t)!==null) {
-      return 1;
+      return true;
     }
   }
-  return 0;
+  return false;
 }
 
+// false if a string is empty, true otherwise
 function nonempty(xs) {
   return !(/^\s*$/.test(xs));
 }
@@ -260,6 +257,7 @@ function syllable(word) {
 }
 
 
+// find a word with x syllables in it
 function getWordOfSyllables(x) {
   var offset = Math.floor(Math.random() * haikuWords.length);
   while(haikuWords[offset]!="" && syllable(haikuWords[offset])!=x) {
@@ -269,6 +267,7 @@ function getWordOfSyllables(x) {
   return haikuWords[offset];
 }
 
+// make a line of n syllables
 function makeLine(n) {
   if(n<1) {return '';}
   var m = 1 + Math.floor(Math.random() * (n-1));
@@ -280,6 +279,7 @@ function makeLine(n) {
   return word + " " + ws;
 }
 
+// make a 5-7-5 syllable(ish) haiku
 function makeHaiku() {
   return makeLine(5) + "<br />\n" + makeLine(7) + "<br />\n" + makeLine(5);
 }
