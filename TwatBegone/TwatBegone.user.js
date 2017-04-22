96d610cf3e3c87b5f8d5f4bc4460f75815a74865
Charlie Harvey <charlie@mallory.hendred.net>
2017-04-22 13:07:31 +0100

    haiku.txt needed adding to git
    Tighten up nav, add TBG.com to it

--- TwatBegone/TwatBegone.user.js
+++ TwatBegone/TwatBegone.user.js
@@ -4,7 +4,7 @@
 // @namespace   ox4
 // @description Removes twats and all mentions of them from twitter
 // @include     https://twitter.com/*
-// @version     0.46
+// @version     0.47
 // @grant       GM_getResourceText
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getValue
